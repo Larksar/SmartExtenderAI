0660cd0fa1f2359d40e54aa7945c6d58975a8e5a
Larksar <angelouk.m30@gmail.com>
2026-06-21 18:35:31 +0100

    created rankings by location df and csv

---
+++ src/Location_Ranking_Table.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+data_dir = "../data"
+results = f"{data_dir}/results"
+
+df = pd.read_csv(f"{results}/All_Location_Summary.csv")
+
+group = df.groupby(["location"])
+
+location_ranking_df = group.agg(
+    mean_rssi = ("mean_rssi", "mean"),
+    std_rssi = ("std_rssi", "mean")
+).reset_index()
+
+location_ranking_df = location_ranking_df.sort_values(
+    by="mean_rssi",
+    ascending=False
+).reset_index(drop=True)
+
+location_ranking_df["recommendation"] =  [
+    "Optimal",
+    "Acceptable",
+    "Poor"
+]
+
+location_ranking_df.insert(
+    0,
+    "rank",
+    range(1, len(location_ranking_df) + 1)
+)
+
+print(location_ranking_df)
+
+location_ranking_df.to_csv(
+    f"{results}/Location_Ranking.csv",
+    index=False
+)
