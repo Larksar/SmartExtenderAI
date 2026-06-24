fbbf69fda853e5620b02a55778b1869164cd2498
Larksar <angelouk.m30@gmail.com>
2026-06-24 13:30:15 +0100

    test

--- visuals/RSSI_Boxplot.py
+++ visuals/RSSI_Boxplot.py
@@ -8,8 +8,6 @@ kitchen_df = pd.read_csv(f"{processed_dir}/Kitchen_Combined.csv")
 living_room_df = pd.read_csv(f"{processed_dir}/Living_Room_Combined.csv")
 upstairs_hallway_df = pd.read_csv(f"{processed_dir}/Upstairs_Hallway_Combined.csv")
 bedroom_df = pd.read_csv(f"{processed_dir}/Bedroom_Combined.csv")
-master_df = pd.read_csv(f"{processed_dir}/Master_Combined.csv")
-spare_room_df = pd.read_csv(f"{processed_dir}/Spare_Room_Combined.csv")
 
 plt.boxplot(
     [
@@ -17,16 +15,12 @@ plt.boxplot(
         living_room_df["rssi"],
         upstairs_hallway_df["rssi"],
         bedroom_df["rssi"],
-        master_df["rssi"],
-        spare_room_df["rssi"]
     ],
     labels=[
         "Kitchen",
         "Living Room",
         "Upstairs Hallway",
         "Bedroom",
-        "Master_Room",
-        "Spare_Room"
     ]
 )
 
