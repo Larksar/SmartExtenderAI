d239e5b0b667eac63d175b625bc85ec301ba166f
Angelo <angelouk.m30@gmail.com>
2026-06-24 13:20:09 +0100

    Feature/location addition (#7)
    * added 3 new locations and updated all other files accordingly, sorted lineage to include folders for cleaner folder/file structure
    
    * fixed viz

---
+++ src/Bedroom_Analysis.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data_dir = "../data"
+processed_dir = f"{data_dir}/processed"
+results_dir = f"{data_dir}/results"
+
+df = pd.read_csv(f"{processed_dir}/Bedroom_Combined.csv")
+
+group = df.groupby(["location", "run_id"])
+
+summary_df = group.agg(
+    mean_rssi = ("rssi", "mean"),
+    min_rssi = ("rssi", "min"),
+    max_rssi = ("rssi", "max"),
+    std_rssi = ("rssi", "std"),
+    sample_count = ("rssi", "count"),
+    detection_rate = ("found", "mean")
+).reset_index()
+
+
+print(summary_df)
+
+summary_df.to_csv(f"{results_dir}/Bedroom_Summary.csv", index=False)
--- visuals/RSSI_Boxplot.py
+++ visuals/RSSI_Boxplot.py
@@ -7,20 +7,30 @@ processed_dir = f"{data_dir}/processed"
 kitchen_df = pd.read_csv(f"{processed_dir}/Kitchen_Combined.csv")
 living_room_df = pd.read_csv(f"{processed_dir}/Living_Room_Combined.csv")
 upstairs_hallway_df = pd.read_csv(f"{processed_dir}/Upstairs_Hallway_Combined.csv")
+bedroom_df = pd.read_csv(f"{processed_dir}/Bedroom_Combined.csv")
+master_df = pd.read_csv(f"{processed_dir}/Master_Combined.csv")
+spare_room_df = pd.read_csv(f"{processed_dir}/Spare_Room_Combined.csv")
 
 plt.boxplot(
     [
         kitchen_df["rssi"],
         living_room_df["rssi"],
-        upstairs_hallway_df["rssi"]
+        upstairs_hallway_df["rssi"],
+        bedroom_df["rssi"],
+        master_df["rssi"],
+        spare_room_df["rssi"]
     ],
     labels=[
         "Kitchen",
         "Living Room",
-        "Upstairs Hallway"
+        "Upstairs Hallway",
+        "Bedroom",
+        "Master_Room",
+        "Spare_Room"
     ]
 )
 
+plt.tight_layout()
 plt.title("RSSI Distribution by Location")
 plt.ylabel("RSSI (dBm)")
 plt.xlabel("Location")
